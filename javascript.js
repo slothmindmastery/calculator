9ab181897130dee6aefaba7975322f93c306ec0c
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-29 00:44:23 +0000

    Create basic calculator buttons and functions
    - Create html file with calculator buttons
    - Add basic layout of buttons in stulesheet using flexbox
    - Add basic javascript calculator function in js file (untested)

---
+++ javascript.js
@@ -0,0 +1,37 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2)  {
+  return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+  if operator == "+" {
+    return add(num1, num2);
+  }
+  else if operator == "-" {
+    return subtract(num1, num2);
+  }
+  else if operator == "*" {
+    return multiply(num1, num2);
+  }
+  else if operator == "/" {
+    return divide(num1, num2);
+  }
+  else {
+    return "Error";
+  }
+}
