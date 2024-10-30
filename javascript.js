a0f1c37dcb28fd9d2a09296b00ae8c72e473d86c
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-30 12:48:06 +0000

    Improve logic of operators
    - event listeners for operator keys updated
    - calculator deals with calculations with more than one operation
    - clearScreenOnNextNumber flag added to clear screen if necessary
    - disableEquals flag added to switch off equals if operator button completes calculation
    - currentOperator reset after equals pressed

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ let num1 = 0;
 let num2 = 0;
 let displayValue = "0";
 let currentOperator = "";
+let clearScreenOnNextNumber = false;
+let disableEquals = false;
 const operators = ["add", "subtract", "multiply", "divide"];
 const buttons = {};
 const display = document.getElementById("display").querySelector("h1");
@@ -10,24 +12,48 @@ const equalsButton = document.getElementById("equals");
 for (let i = 0; i < 10; i++) {
   buttons["button-" + i] = document.getElementById("button-" + i);
   buttons["button-" + i].addEventListener("click", () => {
-    display.textContent = display.textContent + i;
+    if (clearScreenOnNextNumber) {
+      display.textContent = "";
+      clearScreenOnNextNumber = false;
+    }
+    if (display.textContent == 0){
+      display.textContent = i;
+    }
+    else {
+      display.textContent += i;
+    }
     displayValue = Number(display.textContent);
   });
 }
 
 for (operator of operators) {
   const button = document.getElementById(operator);
-  button.addEventListener("click", () => {
-    num1 = Number(display.textContent);
-    display.textContent = "";
+  button.addEventListener("click", (event) => {
+    if (currentOperator && num2 === 0) {
+      num2 = Number(display.textContent);
+      display.textContent = operate(num1, num2, currentOperator);
+      num1 = Number(display.textContent);
+      num2 = 0;
+      disableEquals = true;
+    }
+    else {
+      num1 = Number(display.textContent);
+      disableEquals = false;
+    }
     currentOperator = event.target.id;
+    clearScreenOnNextNumber = true;
   });
 }
 
 equalsButton.addEventListener("click", () => {
-  num2 = Number(display.textContent);
-  display.textContent = operate(num1, num2, currentOperator);
-  num1 = Number(display.textContent);
+  if (disableEquals === false) {
+    num2 = Number(display.textContent);
+    display.textContent = operate(num1, num2, currentOperator);
+    num1 = Number(display.textContent);
+    num2 = 0;
+    currentOperator = "";
+    clearScreenOnNextNumber = true;
+  }
 })
 
 function add(num1, num2) {
@@ -64,13 +90,11 @@ function operate(num1, num2, operator) {
   }
 }
 
-// to do - operator buttons shouldn't clear screen when pressed but screen needs to be cleared sometimes
+
 // clear button needs programming
 // percent button needs programming
-// if you press operator instead of equals, it should display total and then use screen value as first number for new calculation (if statement? variable to show if calculation is in process or not)
 // screen should only store a certain number of digits, answers should only be to so many decimal places
 // decimal point button needs programming
 // decimal point button needs disabling if there is already a decimal point on screen
-// what happens if you press equals too soon?
 // add keyboard support
 // add backspace button?
